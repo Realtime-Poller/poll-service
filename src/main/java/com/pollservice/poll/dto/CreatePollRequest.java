4c55b2f46575750b354dd7a6c320edda34b99a00
konradeconomics <konradkomplexeleistung@gmail.com>
2025-09-03 19:29:28 +0200

    Added Validators to CreatePoll DTO

--- src/main/java/com/pollservice/poll/dto/CreatePollRequest.java
+++ src/main/java/com/pollservice/poll/dto/CreatePollRequest.java
@@ -1,7 +1,13 @@
 package com.pollservice.poll.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class CreatePollRequest {
+    @NotBlank(message = "Title may not be blank.")
+    @Size(max = 200)
     public String title;
 
+    @Size(max = 5000)
     public String description;
 }
